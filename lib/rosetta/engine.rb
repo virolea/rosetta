22950338ac0eceb4c770a0b9203a9ed3277d81d4
Vincent Rolea <3525369+virolea@users.noreply.github.com>
2024-08-30 10:37:53 +0200

    Add assets to precompilation

--- lib/rosetta/engine.rb
+++ lib/rosetta/engine.rb
@@ -1,5 +1,9 @@
 module Rosetta
   class Engine < ::Rails::Engine
     isolate_namespace Rosetta
+
+    initializer "rosetta.assets.precompile" do |app|
+      app.config.assets.precompile += %w[rosetta_manifest.js]
+    end
   end
 end

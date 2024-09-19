aacfeca111c1eee86658e19be650a949c58e6248
Vincent Rolea <3525369+virolea@users.noreply.github.com>
2024-09-19 10:33:07 +0200

    Add demo page in dummy

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+
+  private
+
+  def set_locale
+    if params[:locale]
+      Rosetta.locale = params[:locale]
+    end
+  end
 end
--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -1,11 +1,2 @@
 class PagesController < ApplicationController
-  before_action :set_locale
-
-  private
-
-  def set_locale
-    if params[:locale]
-      Rosetta.locale = params[:locale]
-    end
-  end
 end
